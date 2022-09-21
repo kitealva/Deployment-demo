22d8e088fefe02f63872d3f16de0b365dbbcf79b
Brady <kite.alva@gmail.com>
2022-09-21 12:15:43 -0600

    third commit

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const path = require('path')
-
+require('dotenv').config()
 const port = process.env.port || 5000
 
 const app = express()
+
 app.use(express.json())
 
 app.get('/', (req, res) => {
